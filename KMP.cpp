e3b1932dc4c7f1405fb346a6de406b3d9d8eabc9
Muhtasim Hossain Musanna <114075108+Musanna47@users.noreply.github.com>
2026-06-14 14:35:12 +0600

    2026-06-14 - 001

--- KMP.cpp
+++ KMP.cpp
@@ -5,7 +5,7 @@ using namespace std;
 #define sza(_x) ((int)_x.size())
 
 vector<int> prefix_function(string& s) {
-    int n = (int)s.length();
+    int n = sza(s);
     vector<int> pi(n);
     for (int i = 1; i < n; i++) {
         int j = pi[i - 1];
@@ -17,3 +17,19 @@ vector<int> prefix_function(string& s) {
     }
     return pi;
 }
+
+void compute_automaton(string &s, vector<vector<int>>& aut) {
+    s += '#';
+    int n = s.size();
+    vector<int> pi = prefix_function(s);
+    aut.assign(n, vector<int>(26));
+    for (int i = 0; i < n; i++) {
+        for (int c = 0; c < 26; c++) {
+            if (i > 0 && 'a' + c != s[i])
+                aut[i][c] = aut[pi[i-1]][c];
+            else
+                aut[i][c] = i + ('a' + c == s[i]);
+        }
+    }
+}
+
